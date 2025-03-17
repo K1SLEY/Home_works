3f41954a8f5510681d66557c410f8a867bb19f91
Anton Kiselev <kiselevas859999@gmail.com>
2025-03-17 20:57:19 +0300

    Доюбавил Fraction

--- HW_LINE_2/src/main/java/HW/Main.java
+++ HW_LINE_2/src/main/java/HW/Main.java
@@ -5,34 +5,11 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Point> points = Arrays.asList(new Point(1, 5), new Point(2, 8), new Point(5, 3), new Point(8, 9));
-        PolyLine polyLine = new PolyLine(points);
-        System.out.println("Ломаная, проходящую через точки " + polyLine);
-        double length = polyLine.getLength();
-        System.out.println("Длина ломаной линии: " + length);
-        List<Line> lines = polyLine.getLines();
-        double linesLength = 0;
-        for (Line line : lines) {
-            linesLength += line.getLength();
-        }
-        System.out.println("Длина массива линий: " + linesLength);
+        Fraction f1 = new Fraction(1, 3); // одна треть
+        Fraction f2 = new Fraction(2, 5); // две пятых
+        Fraction f3 = new Fraction(7, 8); // семь восьмых
 
-        if (length == linesLength) {
-            System.out.println("Длины совпадают");
-        } else {
-            System.out.println("Длины не совпадают");
-
-        }
-
-        Point pointToChange = polyLine.getPoints().get(1);
-        pointToChange.setX(12);
-        pointToChange.setY(8);
-        lines = polyLine.getLines();
-        linesLength = 0;
-        for (Line line : lines) {
-            linesLength += line.getLength();
-        }
-        System.out.println("Линии после изменения: " + lines);
-        System.out.println("Длина массива линий после изменения: " + linesLength);
+        Fraction result = f1.sum(f2).sum(f3).minus(5);
+        System.out.println(result); // Результат операции
     }
 }
